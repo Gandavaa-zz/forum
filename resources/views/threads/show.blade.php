c0352af1eef56a8daa55179e7c2ac50d734d9071
Ganaa Mongol <ganaa7@gmail.com>
2019-12-17 08:51:51 +0800

    #04 done reply updated thread added

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -6,7 +6,9 @@
           <div class="columns">                
               <div class="column is-offset-2 is-8">
                     <div class="box" style="margin-top:30px">
-                        <h2 class="title is-4">  {{ $thread->title }} </h2>    
+                        <h2 class="title is-5">
+                            <a href="#">{{ $thread->creator->name }}</a> нийтлэв:
+                              {{ $thread->title }} </h2>    
                         <hr>
                         <div class="thread-body ">
                             <article>
