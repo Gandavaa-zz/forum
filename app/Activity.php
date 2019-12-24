8c9aa5153b87f0d80001a2feb33a97f15b790985
Ganaa Mongol <ganaa7@gmail.com>
2019-12-24 12:57:54 +0800

    #27 Activitiy added feed added here

--- app/Activity.php
+++ app/Activity.php
@@ -13,6 +13,21 @@ class Activity extends Model
     public function subject(){
         
         return $this->morphTo();
-        
+
     }
+
+    // #27 added
+    static function feed($user, $take = 50)
+    {
+        return static::where('user_id', $user->id)
+                ->latest()
+                ->with('subject')
+                ->take($take)
+                ->get()
+                ->groupBy(function ($activity){
+                    return $activity->created_at->format('Y-m-d');
+                });
+    }
+
+
 }
